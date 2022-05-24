655601bc32cc45f1b725222aa43aa57baae3684a
Win Feigle <win.feigle@gmail.com>
2022-05-24 17:34:58 -0600

    commit tree 9013fd0d76d94a9ca77798e89f7cab2b2f488b4b

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
 }
